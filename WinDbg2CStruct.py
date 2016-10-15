55def53c6f34e29879ca697339a3ab041d18f5da
dude719 <blood_099@hotmail.com>
2016-10-15 18:12:59 -0300

    Clean up

--- WinDbg2CStruct.py
+++ WinDbg2CStruct.py
@@ -1,57 +1,8 @@
-
 import os
 import sys
 
 def printf(format, *args):
     sys.stdout.write(format % args)
-
-#
-#nt!_KPROCESS
-#    +0x000 Header           : _DISPATCHER_HEADER
-#    +0x018 ProfileListHead  : _LIST_ENTRY
-#    +0x028 DirectoryTableBase : Uint8B
-#    +0x030 ThreadListHead   : _LIST_ENTRY
-#    +0x040 ProcessLock      : Uint4B
-#    +0x044 Spare0           : Uint4B
-#    +0x048 DeepFreezeStartTime : Uint8B
-#    +0x050 Affinity         : _KAFFINITY_EX
-#    +0x0f8 ReadyListHead    : _LIST_ENTRY
-#    +0x108 SwapListEntry    : _SINGLE_LIST_ENTRY
-#    +0x110 ActiveProcessors : _KAFFINITY_EX
-#    +0x1b8 AutoAlignment    : Pos 0, 1 Bit
-#    +0x1b8 DisableBoost     : Pos 1, 1 Bit
-#    +0x1b8 DisableQuantum   : Pos 2, 1 Bit
-#    +0x1b8 DeepFreeze       : Pos 3, 1 Bit
-#    +0x1b8 TimerVirtualization : Pos 4, 1 Bit
-#    +0x1b8 CheckStackExtents : Pos 5, 1 Bit
-#    +0x1b8 SpareFlags0      : Pos 6, 2 Bits
-#    +0x1b8 ActiveGroupsMask : Pos 8, 20 Bits
-#    +0x1b8 ReservedFlags    : Pos 28, 4 Bits
-#    +0x1b8 ProcessFlags     : Int4B
-#    +0x1bc BasePriority     : Char
-#    +0x1bd QuantumReset     : Char
-#    +0x1be Visited          : UChar
-#    +0x1bf Flags            : _KEXECUTE_OPTIONS
-#    +0x1c0 ThreadSeed       : [20] Uint4B
-#    +0x210 IdealNode        : [20] Uint2B
-#    +0x238 IdealGlobalNode  : Uint2B
-#    +0x23a Spare1           : Uint2B
-#    +0x23c StackCount       : _KSTACK_COUNT
-#    +0x240 ProcessListEntry : _LIST_ENTRY
-#    +0x250 CycleTime        : Uint8B
-#    +0x258 ContextSwitches  : Uint8B
-#    +0x260 SchedulingGroup  : Ptr64 _KSCHEDULING_GROUP
-#    +0x268 FreezeCount      : Uint4B
-#    +0x26c KernelTime       : Uint4B
-#    +0x270 UserTime         : Uint4B
-#    +0x274 LdtFreeSelectorHint : Uint2B
-#    +0x276 LdtTableLength   : Uint2B
-#    +0x278 LdtSystemDescriptor : _KGDTENTRY64
-#    +0x288 LdtBaseAddress   : Ptr64 Void
-#    +0x290 LdtProcessLock   : _FAST_MUTEX
-#    +0x2c8 InstrumentationCallback : Ptr64 Void
-#    +0x2d0 SecurePid        : Uint8B
-
 
 key_types = {
     'UChar': "UCHAR", 
@@ -110,6 +61,7 @@ def get_fields(dt_dump):
             pos, bit = dtype.split(',')
             current_field['bit_pos'] = int(pos[3:])
             current_field['size'] = int(bit[:bit.index('B')])
+            
         # Array field type 
         elif '[' in dtype:
             if "Ptr64" in dtype:
@@ -139,6 +91,7 @@ def get_fields(dt_dump):
                 arr = arr.strip("[]") # drop the brackets
                 current_field['array_size'] = int(arr)
                 current_field['type'] = dt;
+                
         # Pointer field type
         elif "Ptr64" in dtype:
             dtlist = dtype.split(' ')
@@ -156,6 +109,7 @@ def get_fields(dt_dump):
                 else:
                     current_field['type'] = dt
             current_field['size'] = 4
+            
         # Regular field type  
         else:
             current_field['type'] = dtype
